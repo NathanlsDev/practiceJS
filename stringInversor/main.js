9c2039cbb016435a52b10308d0bc5d4d79461b81
Nathan Lima <nathanls.dev@gmail.com>
2022-10-31 19:13:15 -0300

    stringInversor to function

--- stringInversor/main.js
+++ stringInversor/main.js
@@ -1,10 +1,13 @@
-const  string = 'Lógic test'
-const emptyArray = []
-let result = ''
-
-for (i = 0; i < string.length; i++){
-  emptyArray.unshift(string[i])
-  result = emptyArray.join('')
+function invertString (param){
+  const  string = param
+  const emptyArray = []
+  let result = ''
+  
+  for (i = 0; i < string.length; i++){
+    emptyArray.unshift(string[i])
+    result = emptyArray.join('')
+  }
+  console.log(result)
 }
 
-console.log(result)
+invertString('Lógic Test')
