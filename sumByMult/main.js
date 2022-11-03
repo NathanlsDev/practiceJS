2ef5e85f803caf1c9da065cf285e49227abdd124
Nathan Lima <nathanls.dev@gmail.com>
2022-11-03 11:42:12 -0300

    sum multiples

---
+++ sumByMult/main.js
@@ -0,0 +1,14 @@
+function sumByMult (num){
+  const filtered = []
+
+  for (let i = 0; i <= num; i++){
+    if (i % 3 === 0 || i % 5 === 0){
+      filtered.push(i)
+    }
+  }
+
+  const total = filtered.reduce((prev, next) => prev + next)
+  console.log(total)
+}
+
+sumByMult(10)
