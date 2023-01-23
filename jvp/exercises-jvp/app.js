3558087a1d60a06a6d2736f701236cfe86d8021b
Nathan Lima <nathanls.dev@gmail.com>
2023-01-22 21:19:18 -0300

    exercises completed

--- jvp/exercises-jvp/app.js
+++ jvp/exercises-jvp/app.js
@@ -6,19 +6,22 @@
 
   Dica: pesquise por remainder operator (%).
 */
-
 const randomNumbers = [10, 30, 15, 25, 50, 40, 5]
 
 const oddNumbers = randomNumbers.filter( number => number %2 != 0)
 console.log(oddNumbers)
+
 
 /*
   02
 
   - Exiba no console quantos números abaixo de 501 o array abaixo possui.
 */
-
 const crazyNumbers = [937, 5, 395, 402, 501, 333, 502, 781, 3, 691]
+
+const numbersUnder501 = crazyNumbers.filter( number => number < 501)
+console.log(numbersUnder501)
+
 
 /*
   03
@@ -28,8 +31,11 @@ const crazyNumbers = [937, 5, 395, 402, 501, 333, 502, 781, 3, 691]
   
   Dica: pesquise por exponentiation operator (**).
 */
-
 const numbers = [5, 7, 3]
+
+const squaredNumbers = numbers.map( number => number ** 2)
+console.log(squaredNumbers)
+
 
 /*
   04
@@ -38,7 +44,6 @@ const numbers = [5, 7, 3]
     lançados antes do ano 2000;
   - Exiba o novo array no console.
 */
-
 const tarantinoMovies = [
   { name: 'Bastardos inglórios', release: 2009 },
   { name: 'Pulp Fiction', release: 1994 },
@@ -52,13 +57,16 @@ const tarantinoMovies = [
   { name: 'Kill Bill: Volume 1', release: 2003 }
 ]
 
+const moviesBefore2000 = tarantinoMovies.filter( movie => movie.release < 2000)
+console.log(moviesBefore2000)
+
+
 /*
   05
 
   - Gere um novo array que contém apenas os nomes das séries abaixo;
   - Exiba o novo array no console.    
 */
-
 const tvShows = [
   { name: 'Breaking Bad', releaseYear: 2008 },
   { name: 'Mr. Robot', releaseYear: 2015 },
@@ -68,6 +76,10 @@ const tvShows = [
   { name: 'House M.D.', releaseYear: 2004 },
   { name: 'Watchmen', releaseYear: 2019 }
 ]
+
+const namesOfTvShows = tvShows.map(show => show.name)
+console.log(namesOfTvShows)
+
 
 /*
   06
@@ -80,6 +92,11 @@ const tvShows = [
   especial \n.
 */
 
+/*
+- Nome 1
+- Nome 2
+- Nome 3
+*/
 const cart = [
   { name: 'Dark Souls III', price: 95.03 },
   { name: 'Shadow of the Tomb Raider', price: 101.19 },
@@ -88,8 +105,5 @@ const cart = [
   { name: 'Death Stranding', price: 149.99 }
 ]
 
-/*
-- Nome 1
-- Nome 2
-- Nome 3
-*/
+const nameGames = cart.map(games => `- ${games.name}`)
+console.log(nameGames)
